31a74c8f465215edf22903b446a8928018f22e2a
Milindsadanshiv <milindsadanshiv33@gmail.com>
2023-01-21 17:25:26 +0530

    code 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
-        mercedes.move(200, 0);
+        mercedes.move(200, 29);
         mercedes.accelerate(20);
-        mercedes.accelerate(-60);
-
-
+        mercedes.move(20,76);
+//        mercedes.accelerate(-60);
     }
 }
